86a9dd28506104254622cb6d0d7421f2928538af
Jeldert <jeldertook@hotmail.com>
2018-09-20 15:59:56 +0200

    Personal 9-20-2018 16:00

---
+++ JeldertTests/ConsoleApp1/ConsoleApp1/Week1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { string s1 = "Jeldert";
+            //string variabele1 = "C"; //C is een letter
+            //int nummer1 = 123; //123 is een geheel getal
+            //double nummer2 = 1.5; //1.5 is een komma getal
+            //Boolean status = false; //false is een boolean
+            //int nummerconvert = Convert.ToInt32(nummer2);
+            //int rest = nummer1 % nummerconvert;
+            //Console.WriteLine("Hello " + s1);
+            //Console.WriteLine(variabele1 + " " + nummer1);
+            //Console.WriteLine(nummer2 + " " + status);
+            //Console.WriteLine(rest);
+            //Console.ReadKey();
+            //Console.Clear();
+            //Console.WriteLine("geef een temperatuur in Celsius");
+            //int Temperatuur = Convert.ToInt32(Console.ReadLine());
+            //double Fahrenheit = (Temperatuur * 1.8 + 32);
+            //Console.SetCursorPosition(0, 1);
+            //Console.WriteLine(Temperatuur + " Graden Celsius staat gelijk aan " + Fahrenheit + " graden Fahrenheit");
+            //Console.ReadKey();
+            //Console.Clear();
+            //Console.WriteLine("geef een getal op in euro's");
+            //double Euro = Convert.ToDouble(Console.ReadLine());
+            //double Dollar = (Euro * 1.16);
+            //Console.SetCursorPosition(0, 1);
+            //Console.WriteLine(Euro + " Euro is " + Dollar + " USDollar");
+            //Console.ReadKey();
+
+            Console.ReadKey();
+        }
+    }
+}
