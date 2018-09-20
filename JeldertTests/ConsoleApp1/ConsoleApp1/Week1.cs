9856d398b5d5f890d256bc60fa4c37c6f9238914
Jeldert <jeldertook@hotmail.com>
2018-09-20 23:43:47 +0200

    Personal 09-20-2018 23:45
    Update to homework, of no relevance to memory project

--- JeldertTests/ConsoleApp1/ConsoleApp1/Week1.cs
+++ JeldertTests/ConsoleApp1/ConsoleApp1/Week1.cs
@@ -1,42 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConsoleApp1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        { string s1 = "Jeldert";
-            //string variabele1 = "C"; //C is een letter
-            //int nummer1 = 123; //123 is een geheel getal
-            //double nummer2 = 1.5; //1.5 is een komma getal
-            //Boolean status = false; //false is een boolean
-            //int nummerconvert = Convert.ToInt32(nummer2);
-            //int rest = nummer1 % nummerconvert;
-            //Console.WriteLine("Hello " + s1);
-            //Console.WriteLine(variabele1 + " " + nummer1);
-            //Console.WriteLine(nummer2 + " " + status);
-            //Console.WriteLine(rest);
-            //Console.ReadKey();
-            //Console.Clear();
-            //Console.WriteLine("geef een temperatuur in Celsius");
-            //int Temperatuur = Convert.ToInt32(Console.ReadLine());
-            //double Fahrenheit = (Temperatuur * 1.8 + 32);
-            //Console.SetCursorPosition(0, 1);
-            //Console.WriteLine(Temperatuur + " Graden Celsius staat gelijk aan " + Fahrenheit + " graden Fahrenheit");
-            //Console.ReadKey();
-            //Console.Clear();
-            //Console.WriteLine("geef een getal op in euro's");
-            //double Euro = Convert.ToDouble(Console.ReadLine());
-            //double Dollar = (Euro * 1.16);
-            //Console.SetCursorPosition(0, 1);
-            //Console.WriteLine(Euro + " Euro is " + Dollar + " USDollar");
-            //Console.ReadKey();
+//namespace ConsoleApp1
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        { //string s1 = "Jeldert";
+//            string variabele1 = "C"; //C is een letter
+//            int nummer1 = 123; //123 is een geheel getal
+//            double nummer2 = 1.5; //1.5 is een komma getal
+//            Boolean status = false; //false is een boolean
+//            int nummerconvert = Convert.ToInt32(nummer2);
+//            int rest = nummer1 % nummerconvert;
+//            Console.WriteLine("Hello " + s1);
+//            Console.WriteLine(variabele1 + " " + nummer1);
+//            Console.WriteLine(nummer2 + " " + status);
+//            Console.WriteLine(rest);
+//            Console.ReadKey();
+//            Console.Clear();
+//            Console.WriteLine("geef een temperatuur in Celsius");
+//            int Temperatuur = Convert.ToInt32(Console.ReadLine());
+//            double Fahrenheit = (Temperatuur * 1.8 + 32);
+//            Console.SetCursorPosition(0, 1);
+//            Console.WriteLine(Temperatuur + " Graden Celsius staat gelijk aan " + Fahrenheit + " graden Fahrenheit");
+//            Console.ReadKey();
+//            Console.Clear();
+//            Console.WriteLine("geef een getal op in euro's");
+//            double Euro = Convert.ToDouble(Console.ReadLine());
+//            double Dollar = (Euro * 1.16);
+//            Console.SetCursorPosition(0, 1);
+//            Console.WriteLine(Euro + " Euro is " + Dollar + " USDollar");
+//            Console.ReadKey();
+//            Console.WriteLine("Kies je taal");
+//        Start:
+//            Console.WriteLine("Ben je een man of een vrouw?");
+//            string gender = Console.ReadLine();
 
-            Console.ReadKey();
-        }
-    }
-}
+//            if (gender.ToLower() == "man")
+//            {
+//                Console.WriteLine("Vul hier je lengte in");
+//                double Lengte = Convert.ToDouble(Console.ReadLine());
+//                double gewicht = ((Lengte - 100) * 9);
+//                Console.WriteLine("Je ideale gewicht is " + gewicht);
+//            }
+//            else if (gender.ToLower() == "vrouw")
+//            {
+//                Console.WriteLine("Vul hier je lengte in");
+//                double Lengte = Convert.ToDouble(Console.ReadLine());
+//                Console.WriteLine("Vul hier je polsomtrek in");
+//                double Pols = Convert.ToDouble(Console.ReadLine());
+//                double gewicht = ((Lengte + 4 * Pols) / 2);
+//                Console.WriteLine("Je ideale gewicht is " + gewicht);
+//            }
+
+//            else
+//            {
+//                Console.WriteLine("Input niet herkend");
+//                goto Start;
+//            }
+//            Console.ReadKey();
+//        }
+//    }
+//}
---
+++ JeldertTests/ConsoleApp1/ConsoleApp1/Week2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int macht = 2;
+            for (; macht < 1025; macht = macht * 2)
+            {
+                Console.WriteLine("2 raised to the power: {0}", macht);
+                
+            }
+            Console.ReadKey();
+            int getal = 2;
+            while (getal < 1025)
+            {
+                Console.WriteLine("2 raised to the power: {0}", getal);
+                getal = getal * 2;
+            }
+            Console.ReadKey();
+            int number = 2;
+            do
+            {
+                Console.WriteLine("2 raised to the power: {0}", number);
+                number = number * 2;
+            }
+            while (number < 1025);
+            Console.ReadKey();
+        }
+    }
+}
