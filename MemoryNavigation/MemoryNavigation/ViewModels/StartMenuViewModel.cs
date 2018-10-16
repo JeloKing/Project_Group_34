a40a9cbe5c38a108379d92e3ab0ecc2df5360774
CaptainFancyPants <43388288+CaptainFancyPants@users.noreply.github.com>
2018-10-16 10:35:08 +0200

    Main menu navigation started
    Its still needs pass along the grid variable

---
+++ MemoryNavigation/MemoryNavigation/ViewModels/StartMenuViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MemoryNavigation.ViewModels
+{
+    public class StartMenuViewModel
+    {
+
+        private MainWindow _mainWindow;
+        public StartMenuViewModel(MainWindow main)
+        {
+            _mainWindow = main;
+            // MessageBox.Show("test");
+        }
+
+
+        public void StartNewGame()
+        {
+            GameViewModel newGame = new GameViewModel();
+            _mainWindow.DataContext = newGame;
+        }
+
+        
+
+    }
+}
---
+++ MemoryNavigation/MemoryNavigation/Views/MainMenuView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MemoryNavigation.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MemoryNavigation.Views
+{
+    /// <summary>
+    /// Interaction logic for MainMenuView.xaml
+    /// </summary>
+    public partial class MainMenuView : UserControl
+    {
+        // Beschrijf aantal collomen en rijen
+        private const int nr_of_cols = 4;
+        private const int nr_of_rows = 4;
+
+        // Maak de grid
+        GameViewModel grid;
+
+        public MainMenuView()
+        {
+            InitializeComponent();
+        }
+
+
+
+    }
+}
