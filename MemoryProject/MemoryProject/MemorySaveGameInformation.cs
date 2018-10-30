31a38afb5a80f6001295c1926f8ae7b52c1b33aa
CaptainFancyPants <43388288+CaptainFancyPants@users.noreply.github.com>
2018-10-30 20:25:43 +0100

    MemoryProject uploaded to master
    SaveState needs to be implemented still

---
+++ MemoryProject/MemoryProject/MemorySaveGameInformation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryProject
+{
+    public class MemorySaveGameInformation
+    {
+        private string txtdata1;
+        private string txtdata2;
+        private string txtdata3;
+        private string txtdata4;
+        private string txtdata5;
+
+
+        public string Player1Name
+        {
+            get { return txtdata1; }
+            set { txtdata1 = value; }
+        }
+
+        public string Player2Name
+        {
+            get { return txtdata2; }
+            set { txtdata2 = value; }
+        }
+
+        public string Player1Score
+        {
+            get { return txtdata3; }
+            set { txtdata3 = value; }
+        }
+
+        public string Player2Score
+        {
+            get { return txtdata4; }
+            set { txtdata4 = value; }
+        }
+
+        public string ThemeSelected
+        {
+            get { return txtdata5; }
+            set { txtdata5 = value; }
+        }
+
+
+
+
+    }
+}
---
+++ MemoryProject/MemoryProject/SaveMemoryGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace MemoryProject
+{
+    public class SaveMemoryGame
+    {
+
+        public static void savedata(object obj, string filename)
+        {
+
+            XmlSerializer sr = new XmlSerializer(obj.GetType());
+            TextWriter writer = new StreamWriter(filename);
+
+            sr.Serialize(writer, obj);
+            writer.Close();
+        }
+
+    }
+}
