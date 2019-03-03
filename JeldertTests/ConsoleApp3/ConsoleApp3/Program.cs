cc92577bca72b3b18680252689053ba99da5edd1
Jeldert <jeldertook@hotmail.com>
2019-03-03 17:19:45 +0100

    03/03/2019-03

---
+++ JeldertTests/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+            class Person
+            {
+
+            }
+            class Hero : Person
+            {
+
+            }
+            class Villain : Person
+            {
+
+            }
+            class Minion : Person
+            {
+
+            }
+        
+    }
+}
