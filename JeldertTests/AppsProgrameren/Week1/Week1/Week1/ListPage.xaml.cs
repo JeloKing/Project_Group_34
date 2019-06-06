431d0d6b602432ef2182ae366c64a69bf82ca14a
Jeldert <jeldertook@hotmail.com>
2019-06-06 10:43:40 +0200

    06/06/2019

---
+++ JeldertTests/AppsProgrameren/Week1/Week1/Week1/ListPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Week1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListPage : ContentPage
+	{
+		public ListPage (List<string> mainNameList)
+		{
+			InitializeComponent ();
+            NavigationPage.SetHasNavigationBar(this, true);
+
+            nameList.ItemsSource = mainNameList;
+		}
+	}
+}
