5ebcea4f7e83270967322534c3f79d347dfab2a0
Jeldert <jeldertook@hotmail.com>
2019-06-20 16:02:46 +0200

    20/06/2019 16:00

---
+++ JeldertTests/Week1/Week1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Week1
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> names;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            names = new List<string>();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        public void AddClicked(object sender, ClickedEventArgs e)
+        {
+            if (NameEntry.Text == null)
+            {
+                return;
+            }
+            else
+            {
+                names.Add(NameEntry.Text);
+            }
+        }
+
+        public void ShowClicked(object sender, ClickedEventArgs e)
+        {
+            Navigation.PushAsync(new NavigationPage(new ListPage(names)));
+        }
+    }
+}
